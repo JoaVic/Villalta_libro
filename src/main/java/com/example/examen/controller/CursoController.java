00ab06ea879553507429aed88bf8e3e1d4f2aecd
renato <jorgejacinto9701@gmail.com>
2024-06-20 22:51:22 -0500

    Libro terminado

--- src/main/java/com/example/examen/controller/CursoController.java
+++ src/main/java/com/example/examen/controller/CursoController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.examen.entity.Curso;
 import com.example.examen.service.CursoService;
+
 
 
 @RestController
@@ -24,4 +26,16 @@ public class CursoController {
 	public List<Curso> lista(){
 		return service.listaCurso();
 	}
+	@GetMapping("/buscaCursoPorId/{id}")
+	@ResponseBody
+	public Curso buscaCursoPorId(@PathVariable("id") int idCurso) {
+		return service.buscaCursoPorId(idCurso);
+	}
+
+	@GetMapping("/buscaCursoPorNombre/{var}")
+	@ResponseBody
+	public List<Curso>buscaCursoPorNombre(@PathVariable("var") String nombre) {
+		return service.buscaCursoPorNombre(nombre);
+	}
+
 }
---
+++ src/main/java/com/example/examen/entity/Libro.java
@@ -0,0 +1,21 @@
+package com.example.examen.entity;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Document(collection = "libro")
+public class Libro {
+    
+    @Id
+    private String id;
+    private String title;
+    private String status;
+    private List<String> categories;
+}
